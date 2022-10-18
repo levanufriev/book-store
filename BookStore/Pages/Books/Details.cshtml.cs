96240806976be703b0e1b517224332484568fbae
levanufriev02 <levanufriev02@gmail.com>
2022-10-18 09:47:20 +0300

    Add book pages.

---
+++ BookStore/Pages/Books/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BookStore.DataLayer;
+using BookStore.Models;
+
+namespace BookStore.Pages.Books
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly BookStore.DataLayer.BookDbContext _context;
+
+        public DetailsModel(BookStore.DataLayer.BookDbContext context)
+        {
+            _context = context;
+        }
+
+      public Book Book { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Books == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books.FirstOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Book = book;
+            }
+            return Page();
+        }
+    }
+}
---
+++ BookStore/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BookStore.DataLayer;
+using BookStore.Models;
+
+namespace BookStore.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly BookStore.DataLayer.BookDbContext _context;
+
+        public IndexModel(BookStore.DataLayer.BookDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Books != null)
+            {
+                Book = await _context.Books.ToListAsync();
+            }
+        }
+    }
+}

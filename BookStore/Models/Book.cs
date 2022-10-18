a57176c67bfea2e5a9abcbb87d49d2ba401b8c17
levanufriev02 <levanufriev02@gmail.com>
2022-10-18 10:20:58 +0300

    Add validation.

--- BookStore/Models/Book.cs
+++ BookStore/Models/Book.cs
@@ -6,9 +6,28 @@ namespace BookStore.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Title name is too short.")]
         public string Title { get; set; }
+
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Author name is too short.")]
         public string Author { get; set; }
+
+        [Display(Name = "Release date")]
+        [DataType(DataType.Date)]
+        [DateValidation(ErrorMessage = "Date should be less than current date.")]
         public DateTime ReleaseDate { get; set; }
+
+        [Range(1, 5, ErrorMessage = "Rating should be from 1 to 5.")]
         public string Rating { get; set; }
+    }
+
+    public class DateValidation : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            DateTime date = Convert.ToDateTime(value);
+            return date.Date <= DateTime.Now;
+        }
     }
 }
